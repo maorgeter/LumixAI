442b452d89bc6c53ab9bff8658a724542219ecd8
Maor Geter <maorgetter@gmail.com>
2022-11-24 19:02:18 +0200

    add extensions and google upload almost ready

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import tkinter as Tk
+from tkinter.filedialog import askopenfilename
+from pathlib import Path
+import os
+ 
 
 # Hello messages
 print("********************\nHello and welcome to LumixAI uploader\n********************\n")
@@ -20,9 +25,18 @@ while(not checker):
     val = input("Your choose: ")
     match val:
         case "1":
-            print("google")
+            # Tk().withdraw()
+            gauth = GoogleAuth()           
+            drive = GoogleDrive(gauth)
+            filePath = askopenfilename()
+            print(Path(filePath).stem + os.path.splitext(filePath)[-1]) 
+            gfile = drive.CreateFile({'title': 'Hello.txt'})
+            gfile.SetContentFile('./maor.txt')
+            gfile.Upload()
+            checker = True
         case "2":
             print("Mega")
+            checker = True
         case _:
             if(val != str(menuLength+1)):
                 print("Wrong option. Please try again.")
