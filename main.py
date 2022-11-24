1f1aaa6b1de4341b8624cc0b8f267cab1c8b21be
Maor Geter <maorgetter@gmail.com>
2022-11-24 19:35:07 +0200

    finish list option

--- main.py
+++ main.py
@@ -6,40 +6,48 @@ from pathlib import Path
 import os
  
 
-# Hello messages
-print("********************\nHello and welcome to LumixAI uploader\n********************\n")
-print("Please choose where do you want upload your file:")
-
 # Menu options
-menu = ["Google Drive", "Mega.nz" ]
-checker = False
+menu = ["Upload new file", "Download file", "List all files" ]
 menuLength = len(menu)
 
-# Menu builder
-for i in range (menuLength):
-    print(str((i+1)) + ". " + menu[i])
-print(str(menuLength+1) + ". Exit")
 
-# User choose menu options
-while(not checker):
-    val = input("Your choose: ")
-    match val:
-        case "1":
-            # Tk().withdraw()
-            gauth = GoogleAuth()           
-            drive = GoogleDrive(gauth)
-            filePath = askopenfilename()
-            print(Path(filePath).stem + os.path.splitext(filePath)[-1]) 
-            gfile = drive.CreateFile({'title': 'Hello.txt'})
-            gfile.SetContentFile('./maor.txt')
-            gfile.Upload()
-            checker = True
-        case "2":
-            print("Mega")
-            checker = True
-        case _:
-            if(val != str(menuLength+1)):
-                print("Wrong option. Please try again.")
-            else:
-                print("Bye bye :)")
-                quit()
+def printUploadMenu():
+    for i in range (menuLength):
+        print(str((i+1)) + ". " + menu[i])
+    print(str(menuLength+1) + ". Exit")
+
+def googleDriveChooser():
+    while True:
+        printUploadMenu()
+        val = input("Your choose: ")
+        gauth = GoogleAuth()           
+        drive = GoogleDrive(gauth)
+        match val:
+            case "1":
+                # Tk().withdraw()
+                filePath = askopenfilename()
+                fileName = Path(filePath).stem + os.path.splitext(filePath)[-1] 
+                gfile = drive.CreateFile({'title': fileName})
+                gfile.SetContentFile(filePath)
+                gfile.Upload()
+                print("\n********************\n" + fileName + " successfully uploaded to Google Drive!\n********************\n\n")
+            case "2":
+                print("download")
+                printUploadMenu()
+            case "3":
+                file_list = drive.ListFile().GetList()
+                print("\n********************\n")
+                for file in file_list:
+                    print("Name: %s" % (file['title']))
+                print("\n********************\n\n")
+            case _:
+                if(val != str(menuLength+1)):
+                    print("\n********************\n Wrong option. Please try again!\n********************\n\n")
+                else:
+                    print("\n********************\n Bye bye :)\n********************\n\n")
+                    quit()
+
+# Hello messages
+print("********************\nHello and welcome to LumixAI GoogleDrive tool\n********************\n")
+print("Please choose one option:")
+googleDriveChooser()
